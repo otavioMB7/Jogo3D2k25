d2c90319c0bf0ffbb04ab337ab5c390d96abbb03
joaopedrooooo77 <joao.233219@alunosatc.edu.br>
2025-05-08 17:24:31 -0300

    aaa

--- My project/Assets/Player/Script/PlayerMov.cs
+++ My project/Assets/Player/Script/PlayerMov.cs
@@ -1,58 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 
 public class PlayerMov : MonoBehaviour
 {
-    public float Speed;
-    public float RotSpeed;
-    private float Rotation;
-    public float Gravity;
+    private CharacterController controller;
+    private Transform Mycamera;
 
-    Vector3 MoveDirection;
-    CharacterController controller;
-    Animator anim;
-
-
-    // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        anim= GetComponent<Animator>();
+        Mycamera = Camera.main.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Move();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movimento = new Vector3(horizontal, 0, vertical);
+
+        movimento  = Mycamera.TransformDirection(movimento);
+        movimento.y = 0;
+
+        controller.Move(movimento * Time.deltaTime * 5  );
+        controller.Move(new Vector3 (0, - 9.81f, 0) * Time.deltaTime);
     }
-
-    void Move()
-    {
-        if (controller.isGrounded)
-        {
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                MoveDirection = Vector3.forward * Speed;
-                MoveDirection = transform.TransformDirection(MoveDirection);
-                anim.SetInteger("transition", 1);
-            }
-            if (Input.GetKeyUp(KeyCode.W))
-            {
-                MoveDirection = Vector3.zero;
-                anim.SetInteger("transition", 0);
-                //transform.Translate(Vector3.back * Velocity * Time.deltaTime);
-            }
-        }
-        Rotation += Input.GetAxis("Horizontal") * RotSpeed * Time.deltaTime;
-        transform.eulerAngles = new Vector3(0, Rotation, 0);
-
-        MoveDirection.y -= Gravity * Time.deltaTime;
-        MoveDirection = transform.TransformDirection(MoveDirection);
-        controller.Move(MoveDirection * Time.deltaTime);
-    }
-
 }
